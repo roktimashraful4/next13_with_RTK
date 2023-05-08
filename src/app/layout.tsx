fa7f08050560c3cd6f6ffeeec1c6c5969d9dee77
Roktim Ashraful <roktimashraful4@gmail.com>
2023-05-08 23:09:18 +0600

    fix indantant

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Providers>
-      {children}
-      </Providers>
-        </body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
